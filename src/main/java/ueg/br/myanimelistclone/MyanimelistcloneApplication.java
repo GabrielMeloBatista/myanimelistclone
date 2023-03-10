0406f127a778ef8e3bd50bd8f45d63f5e9292f60
GabrielMeloBatista <gabrielmelobatisa@hotmail.com>
2023-03-09 23:23:19 -0300

    Initial commit

---
+++ src/main/java/ueg/br/myanimelistclone/MyanimelistcloneApplication.java
@@ -0,0 +1,13 @@
+package ueg.br.myanimelistclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyanimelistcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyanimelistcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ueg/br/myanimelistclone/MyanimelistcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package ueg.br.myanimelistclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyanimelistcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
